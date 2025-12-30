a4311a4b51b00c78b856f6ee92bacf19cc27b319
kartikey <kartikeyrao43@gmail.com>
2025-12-30 23:17:29 +0530

    Solved 239 Sliding Window Maximum

---
+++ SlidingWindowMax.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class SlidingWindowMax {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove elements out of window
+            if (!dq.isEmpty() && dq.peekFirst() == i - k) {
+                dq.pollFirst();
+            }
+
+            // Remove smaller elements from back
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            dq.offerLast(i);
+
+            // Store result once window is valid
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[dq.peekFirst()];
+            }
+        }
+        return result;
+    }
+}
