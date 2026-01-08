a2b87aab4049b221c3224ff8c6f30c8ca346bb6c
kartikey <kartikeyrao43@gmail.com>
2026-01-08 20:56:12 +0530

    Solved 144 Binary Tree Pre order traversal

---
+++ BinaryTreePreOrderTraversal.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class BinaryTreePreOrderTraversal {
+     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(root,result);
+        return result;
+    }
+
+    private void preorder(TreeNode node, List<Integer> result){
+        if(node == null) return;
+        result.add(node.val);
+        preorder(node.left,result);
+        preorder(node.right, result);
+    }
+}
