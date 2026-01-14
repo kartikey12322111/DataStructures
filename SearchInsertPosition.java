de635ab6d99b39bcae690652b55b807f2bada3d5
kartikey <kartikeyrao43@gmail.com>
2026-01-14 19:23:01 +0530

    Solved 35 Search Insert Position

---
+++ SearchInsertPosition.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class SearchInsertPosition {
+    public int longestConsecutive(int[] nums) {
+        if(nums == null || nums.length==0){
+            return 0;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longestSequence = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentLength = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentLength++;
+                }
+
+                longestSequence = Math.max(longestSequence, currentLength);
+            }
+        }
+
+        return longestSequence;
+    }
+}
