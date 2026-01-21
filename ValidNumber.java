1b38f5ae49e556a82f54881d7990f22615481f17
kartikey <kartikeyrao43@gmail.com>
2026-01-21 21:59:11 +0530

    Solved 65 Valid Number

---
+++ ValidNumber.java
@@ -0,0 +1,28 @@
+public class ValidNumber {
+    public boolean isNumber(String s) {
+        s = s.trim(); 
+        boolean seenDigit = false, seenDot = false, seenE = false;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                seenDigit = true;
+            } else if (c == '.') {
+                if (seenDot || seenE) return false; 
+                seenDot = true;
+            } else if (c == 'e' || c == 'E') {
+                if (seenE || !seenDigit) return false; 
+                seenE = true;
+                seenDigit = false;
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
+            } else {
+                return false; 
+            }
+        }
+
+        return seenDigit; 
+    }
+}
