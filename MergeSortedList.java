424082d5f4d4a1b1ac0fc9330563b003e3b4fede
kartikey <kartikeyrao43@gmail.com>
2026-01-31 22:06:43 +0530

    Solved 21 Mergee Sorted List

---
+++ MergeSortedList.java
@@ -0,0 +1,49 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+
+    public void printList(ListNode head) {
+        ListNode current = head;
+        while(current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        Solution solution = new Solution();
+        ListNode merged = solution.mergeTwoLists(l1, l2);
+
+        System.out.print("Merged List: ");
+        solution.printList(merged); 
+    }
+}
