4c1a3503fc565f51b006b5001a22f984a1d67805
kartikey <kartikeyrao43@gmail.com>
2026-02-02 16:49:34 +0530

    Solved 203 Remove Linked List Elements

---
+++ RemoveLinkedListElements.java
@@ -0,0 +1,31 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        ListNode temp = head;
+
+        while (temp != null && temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
