61e1a9fb5593a2a5e771782f63815d3a1c08090a
kartikey <kartikeyrao43@gmail.com>
2026-02-13 07:16:31 +0530

    Solved 1147 Remove all adjacent duplicate in string

---
+++ RemoveAllAdjDuplicateInString.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class RemoveAllAdjDuplicateInString {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(!st.isEmpty() && st.peek() == ch){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(char ch: st){
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
