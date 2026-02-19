10f49497e76901d9c2b64838e7c130a8ac5bbae5
kartikey <kartikeyrao43@gmail.com>
2026-02-19 20:06:52 +0530

    Solved 225 Implement stack using queues

---
+++ MyStack.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+    private Queue<Integer>main;
+    private Queue<Integer>helper;
+    public MyStack() {
+        main = new LinkedList<>();
+        helper = new LinkedList<>();
+
+    }
+    
+    public void push(int x) {
+        while(main.size()>0){
+            helper.add(main.remove());
+        }
+        main.add(x);
+
+        while(helper.size()>0){
+            main.add(helper.remove());
+        }
+    }
+    
+    public int pop() {
+        return main.remove();
+    }
+    
+    public int top() {
+        return main.peek();
+    }
+    
+    public boolean empty() {
+        if(main.size()==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
