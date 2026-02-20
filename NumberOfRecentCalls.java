68caabea51becbb11e58392429c35f0151312a9c
kartikey <kartikeyrao43@gmail.com>
2026-02-20 11:33:04 +0530

    Solved 933 Number of recent calls

---
+++ NumberOfRecentCalls.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;         
+
+
+public class NumberOfRecentCalls {
+    Queue<Integer> queue;
+
+    public NumberOfRecentCalls() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+
+        while (!queue.isEmpty() && queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
