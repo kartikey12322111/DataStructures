0d58069d6df1b8be8c87ea74d29e0e522465c8ca
kartikey <kartikeyrao43@gmail.com>
2026-04-01 19:55:56 +0530

    Solved 47 Permutation2

---
+++ Permutation2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutation2 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, used, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> temp, List<List<Integer>> res) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            temp.add(nums[i]);
+
+            backtrack(nums, used, temp, res);
+
+            used[i] = false;
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
