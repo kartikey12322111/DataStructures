a92fa69fa8da90a70b76c5173d20490cf99ffdd6
kartikey <kartikeyrao43@gmail.com>
2026-04-12 21:53:49 +0530

    Solved 61 Rotate List

---
+++ RotateList.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+import java.util.HashMap;
+import java.util.Map;   
+
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        tail.next = head;
+        k = k % length;
+
+        int stepsToNewTail = length - k - 1;
+        ListNode newTail = head;
+
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
